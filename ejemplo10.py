d5409e39cc4070f6aa213c9f07061e491a363e3a
ByronCast09 <byronjosue14z@gmail.com>
2024-04-29 22:30:23 -0500

    deber

--- ejemplo10.py
+++ ejemplo10.py
@@ -4,9 +4,9 @@ archivo= open("data/atp_tennis.csv","r")
 lineas = archivo.readlines()
 lineas= [l.split("|") for l in lineas]
 
-for x in lineas:
+for b, x in enumerate(lineas):
 	cadena = """<b>Torneo:</b> %s <br> <b>Ganador:</b> %s""" % (x[0],x[9])
 	print(cadena)
-	archivo_generado = open("data/%s.html" % (x[9]),"w")# nombre unico de archivo hacer, deben salir 25362 archivos con nombres unicos
+	archivo_generado = open("data/%s_%s.html" % (x[9],b),"w")# nombre unico de archivo hacer, deben salir 25362 archivos con nombres unicos
 	archivo_generado.writelines("%s\n" % (cadena))
 	archivo_generado.close()
