823166c0e4f31331e59790ddf15604e4b8e682f3
ByronCast09 <byronjosue14z@gmail.com>
2024-04-29 19:43:56 -0500

    avances

---
+++ ejemplo03.py
@@ -0,0 +1,9 @@
+nombre= input("INgrese su nombre:")
+
+edad =int(input("INgrese la edad de las personas:"))
+
+sueldo = float(input("INgrese el sueldo de la persona:"))
+
+mensajeFInal = "Nombre:%s\nEdad:%d\nSueldo:%.2f\n"%(nombre,edad,sueldo)
+
+print(mensajeFInal)
---
+++ ejemplos02.py
@@ -0,0 +1,6 @@
+mensaje="Hola Mundo"
+
+for x in range (0,10):
+	print("%s"% (mensaje))
+
+print("FIn de mensaje")
